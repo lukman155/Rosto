0c31d6bd0d718fe8ee04f43c710747f6f6cc40b0
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2022-11-23 15:19:45 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,24 +11,55 @@ const getMeals = async (url) => {
   return data.json();
 };
 
+const displayPopup = () => {
+  console.log('displayPopup');
+};
+
 const render = (data) => {
   const container = document.querySelector('.container');
   container.innerHTML = '';
   data.forEach((item) => {
-    container.innerHTML += `
-    <li class='card'>
-    <div class='img-holder'>
-      <img src='${item.strMealThumb}' alt='' />
-    </div>
-    <div class="info">
-      <h2 class="card-title">${item.strMeal}</h2>
-      <button onclick=''
-      )}' class='explore'>Explore more</button>
-    </div>
-  </li>
-`;
+    const card = document.createElement('li');
+    card.classList.add('card');
+    card.innerHTML = `
+      <div class='img-holder'>
+      <img src='${item.strMealThumb}' alt='${item.strMeal}' />
+      </div>`;
+    const info = document.createElement('div');
+    info.classList.add('info');
+    const title = document.createElement('h2');
+    title.classList.add('card-title');
+    title.textContent = item.strMeal;
+
+    const btn = document.createElement('button');
+    btn.classList.add('explore');
+    btn.textContent = 'View Recipe';
+    btn.addEventListener('click', () => {
+      displayPopup();
+    });
+
+    info.appendChild(title);
+    info.appendChild(btn);
+    card.appendChild(info);
+    container.appendChild(card);
   });
 };
+
+//   data.forEach((item) => {
+//     container.innerHTML += `
+//     <li class='card'>
+//     <div class='img-holder'>
+//       <img src='${item.strMealThumb}' alt='' />
+//     </div>
+//     <div class="info">
+//       <h2 class="card-title">${item.strMeal}</h2>
+//       <button onclick=''
+//       )}' class='explore'>Explore more</button>
+//     </div>
+//   </li>
+// `;
+//   });
+// };
 
 document.addEventListener('DOMContentLoaded', async () => {
   const data = await getMeals(baseURl);
