ef0d68de8081268558c56d673a383a3025f9d927
Lukman Abdulkarim <lukmanabdulkarim15@gmail.com>
2022-11-25 11:03:47 +0000

    Revert "Implement: Likes Functionality"

--- src/index.js
+++ src/index.js
@@ -2,16 +2,9 @@ import './style.css';
 import logo from './images/Rosto.jpg';
 import closeIcon from './images/x.svg';
 
-import { postLikes, getLikes, renderLikes } from './modules/likes.js';
-import mealCounter from './modules/counter.js';
-import { getMeals, baseURl } from './modules/requests.js';
-
-
-
 const logoImg = document.querySelector('.logo');
 const closeImg = document.querySelector('.close');
 logoImg.src = logo;
-
 closeImg.src = closeIcon;
 
 const handelBase = (category = 'Seafood') => {
@@ -36,50 +29,16 @@ const postData = async (data = {}) => {
   return postedData;
 };
 
+const getMeals = async (url) => {
+  const data = await fetch(url);
+  return data.json();
+};
 
-const likeTemplate = `
-      <svg id="heart-svg" viewBox="467 392 58 57" xmlns="http://www.w3.org/2000/svg">
-        <g id="Group" fill="none" fill-rule="evenodd" transform="translate(467 392)">
-          <path id="heart" d="M29.144 20.773c-.063-.13-4.227-8.67-11.44-2.59C7.63 28.795 28.94 43.256 29.143 43.394c.204-.138 21.513-14.6 11.44-25.213-7.214-6.08-11.377 2.46-11.44 2.59z" fill="#AAB8C2"/>
-          <circle id="main-circ" fill="#E2264D" opacity="0" cx="29.5" cy="29.5" r="1.5"/>
+const getCommentsList = async (id) => {
+  const comments = await fetch(`${commentsURL}?item_id=${id}`);
+  return comments.json();
+};
 
-          <g id="grp7" opacity="0" transform="translate(7 6)">
-            <circle id="oval1" fill="#9CD8C3" cx="2" cy="6" r="2"/>
-            <circle id="oval2" fill="#8CE8C3" cx="5" cy="2" r="2"/>
-          </g>
-
-
-          <g id="grp6" opacity="0" transform="translate(0 28)">
-            <circle id="oval1" fill="#CC8EF5" cx="2" cy="7" r="2"/>
-            <circle id="oval2" fill="#91D2FA" cx="3" cy="2" r="2"/>
-          </g>
-
-          <g id="grp3" opacity="0" transform="translate(52 28)">
-            <circle id="oval2" fill="#9CD8C3" cx="2" cy="7" r="2"/>
-            <circle id="oval1" fill="#8CE8C3" cx="4" cy="2" r="2"/>
-          </g>
-
-          <g id="grp2" opacity="0" transform="translate(44 6)">
-            <circle id="oval2" fill="#CC8EF5" cx="5" cy="6" r="2"/>
-            <circle id="oval1" fill="#CC8EF5" cx="2" cy="2" r="2"/>
-          </g>
-
-          <g id="grp5" opacity="0" transform="translate(14 50)">
-            <circle id="oval1" fill="#91D2FA" cx="6" cy="5" r="2"/>
-            <circle id="oval2" fill="#91D2FA" cx="2" cy="2" r="2"/>
-          </g>
-
-          <g id="grp4" opacity="0" transform="translate(35 50)">
-            <circle id="oval1" fill="#F48EA7" cx="6" cy="5" r="2"/>
-            <circle id="oval2" fill="#F48EA7" cx="2" cy="2" r="2"/>
-          </g>
-
-          <g id="grp1" opacity="0" transform="translate(24)">
-            <circle id="oval1" fill="#9FC7FA" cx="2.5" cy="3" r="2"/>
-            <circle id="oval2" fill="#9FC7FA" cx="7.5" cy="2" r="2"/>
-          </g>
-        </g>
-      </svg>`;
 const displayCommentsList = async (id) => {
   const data = await getCommentsList(id);
   const commentsList = document.querySelector('.comments-list');
@@ -115,14 +74,12 @@ const displayPopup = (item, id) => {
   displayCommentsList(id);
 };
 
-
 const render = async (data) => {
   const container = document.querySelector('.container');
   container.innerHTML = '';
   data.meals.forEach((item) => {
     const card = document.createElement('li');
     card.setAttribute('data-set', item.idMeal);
-
     card.classList.add('card');
     card.innerHTML = `
       <div class='img-holder'>
@@ -134,54 +91,22 @@ const render = async (data) => {
     title.classList.add('card-title');
     title.textContent = item.strMeal;
 
-
-    const likeContainer = document.createElement('div');
-    likeContainer.classList.add('like-container');
-
-    const likeInput = document.createElement('input');
-    likeInput.type = 'checkbox';
-    likeInput.setAttribute('id', `${item.idMeal}`);
-    likeInput.classList.add('checkbox');
-    likeInput.addEventListener('change', () => {
-      if (likeInput.checked) {
-        postLikes(item.idMeal);
-        getLikes(item.idMeal);
-      }
-    });
-
-    const label = document.createElement('label');
-    label.setAttribute('for', `${item.idMeal}`);
-    label.innerHTML = likeTemplate;
-
-    const noLikes = document.createElement('span');
-    noLikes.classList.add('no-likes');
-    noLikes.classList.add(`a${item.idMeal}`);
-    noLikes.textContent = '';
-
     const btn = document.createElement('button');
     btn.classList.add('explore');
     btn.textContent = 'View Recipe';
-     btn.addEventListener('click', async () => {
+    info.append(title, btn);
+    card.appendChild(info);
+    container.appendChild(card);
+    btn.addEventListener('click', async () => {
       const cardId = btn.parentElement.parentElement.dataset.set;
       displayPopup(item, cardId);
     });
-    
-    info.appendChild(title);
-    likeContainer.appendChild(likeInput);
-    likeContainer.appendChild(label);
-    likeContainer.appendChild(noLikes);
-    likeContainer.appendChild(btn);
-    info.appendChild(likeContainer);
-    card.appendChild(info);
-    container.appendChild(card);
   });
 };
 
 document.addEventListener('DOMContentLoaded', async () => {
   const getData = await getMeals(handelBase());
   render(getData);
-  mealCounter();
-  renderLikes();
 });
 
 document.querySelectorAll('.nav-link').forEach((link) => {
--- src/modules/counter.js
+++
@@ -1,7 +0,0 @@
-const mealCounter = () => {
-  const container = document.querySelector('.container');
-  const mealcounter = document.querySelector('.meal-count');
-  mealcounter.innerHTML = `(${container.children.length} recipes available)`;
-};
-
-export default mealCounter;
