c8f773ca85bfc36902cb6f81c083aba46e9a61dd
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2022-11-25 17:04:13 +0100

    Add Tests for Item Counter

---
+++ src/modules/counter.test.js
@@ -0,0 +1,22 @@
+/** @jest-environment jsdom */
+import mealCounter from './counter.js';
+import '@testing-library/jest-dom';
+
+describe('mealCounter', () => {
+    beforeEach(() => {
+        document.body.innerHTML = `
+        <div class="meal-count"></div>
+        <div class="container">
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+        </div>`;
+        mealCounter();
+    });
+    test('should display the number of meals available', () => {
+        expect(document.querySelector('.meal-count').innerHTML).toBe('(6 recipes available)');
+    });
+});
